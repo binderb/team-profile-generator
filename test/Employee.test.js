31732bfa216835325ecc8f43ca68917e1f6bb0e4
Ben Binder <ben.binder@switchbackmedical.com>
2022-11-02 11:56:26 -0500

    Implement first test for Employee.

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,7 +1,12 @@
 const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
-  it('New object should contain a name, id, and email', () => {
-    
+  it('New object should contain the assigned name, id, and email', () => {
+    // arrange and act
+    const new_employee = new Employee('Ben',0,'ben@example.com');
+    // assert
+    expect(new_employee.name).toEqual('Ben');
+    expect(new_employee.id).toEqual(0);
+    expect(new_employee.email).toEqual('ben@example.com');
   });
 });
